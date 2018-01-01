6bf13091f16f48e91301cc4660417217e2d67ea1
Oliver <oliver.nic012@gmail.com>
2018-01-01 17:33:12 -0600

    Updated webpack config
    Node bundle is now included.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,38 @@
 var packagejson = require("./package.json");
-var name = packagejson.name;
-var version = packagejson.version;
-var filenameExtension = (process.argv.indexOf('--optimize-minimize') !== -1) ? ".min.js" : ".js";
-module.exports = {
-    entry: "./src/index.ts",
-    output: {
-        filename: name + "-" + version + filenameExtension,
-        path: __dirname + "/dist/CornerStone-" + version,
-        library: 'CornerStone',
-        libraryTarget: 'umd'
-    },
+function createConfig(target) {
+   var name = packagejson.name;
+   var version = packagejson.version;
+   var filenameExtension = (process.argv.indexOf('--optimize-minimize') !== -1) ? ".min.js" : ".js";
+   return {
+      target: target,
+      entry: "./src/index.ts",
+      output: {
+            filename: name + "-" + version + "." + target + filenameExtension,
+            path: __dirname + "/dist/CornerStone-" + version,
+            library: 'CornerStone',
+            libraryTarget: 'umd'
+      },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+      // Enable sourcemaps for debugging webpack's output.
+      devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+      resolve: {
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: [".ts", ".tsx", ".js", ".json"]
+      },
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      module: {
+            rules: [
+                  // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+                  { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    }
-};
+                  // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                  { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            ]
+      }
+   };
+}
+webConfig = createConfig("web");
+nodeConfig = createConfig("node");
+
+module.exports = [webConfig, nodeConfig];
