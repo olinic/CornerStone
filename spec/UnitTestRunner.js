9e2c338ad06d1238e9f90dbeb6e912484ce01326
Oliver Nichols <oliver.nic012@gmail.com>
2019-01-14 21:18:46 -0600

    Initial setup of unit test runner.

--- spec/UnitTestRunner.js
+++ spec/UnitTestRunner.js
@@ -1,4 +1,24 @@
+const fs = require('fs');
+const { spawn } = require('child_process');
 const Jasmine = require('jasmine');
+
+//------------------------------------------------------------------------------
+// Setup server
+const serverOutFile = "./out.log";
+const out = fs.openSync(serverOutFile, 'w');
+const err = fs.openSync(serverOutFile, 'w');
+console.log("Look at " + serverOutFile + " for server output.");
+
+const serverDuration = 15; // in seconds
+
+// Start server in background
+const subprocess = spawn('node', ['./ExpressServer/server.js', serverDuration], {
+  detached: true,
+  stdio: [ 'ignore', out, err ]
+});
+
+//------------------------------------------------------------------------------
+// Setup Jasmine
 let jasmine = new Jasmine();
 jasmine.loadConfig(
    {
@@ -15,4 +35,5 @@ jasmine.loadConfig(
    }
 );
 
+// Start Jasmine
 jasmine.execute();
